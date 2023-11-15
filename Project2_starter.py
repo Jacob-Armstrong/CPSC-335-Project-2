8305c38851979854da9679e8ad3af788f8531b67
Jacob <silverlock99@gmail.com>
2023-11-15 11:11:25 -0800

    Change variable names for consistency

--- Project2_starter.py
+++ Project2_starter.py
@@ -9,41 +9,41 @@ def Parse_File():
         # Number of stocks
         n_index = section.find('N = ')
         n_end = section.find('\n', n_index)
-        N = int(section[n_index + len('N = '):n_end].strip())
+        n = int(section[n_index + len('N = '):n_end].strip())
 
         # Stocks and values
         stocks_index = section.find('Stocks and values = ')
         stocks_end = section.find('].', stocks_index)
         stocks_str = section[stocks_index + len('Stocks and values = '):stocks_end + 1]
-        stocks_and_values = eval(stocks_str)
+        stocks = eval(stocks_str)
 
-        # Convert the stocks_and_values into arrays of numbers
-        stocks_and_values = [list(map(int, pair)) for pair in stocks_and_values]
+        # Convert the stocks into arrays of numbers
+        stocks = [list(map(int, pair)) for pair in stocks]
 
         # Amount of resources
         amount_index = section.find('Amount = ')
         amount_end = section.find('\n', amount_index)
-        Amount = int(section[amount_index + len('Amount = '):amount_end].strip())
+        amount = int(section[amount_index + len('Amount = '):amount_end].strip())
 
         # Print the extracted values for each section
         print(f"Sample {i}:")
-        print("N:", N)
-        print("Stocks and Values:", stocks_and_values)
-        print("Amount:", Amount)
+        print("N:", n)
+        print("Stocks and Values:", stocks)
+        print("Amount:", amount)
         print("------")
 
         print("Beginning Exhaustive Search implementation for sample input " + str(i))
-        Exhaustive_Search(N, stocks_and_values, Amount)
+        Exhaustive_Search(n, stocks, amount)
     
         print("Beginning Dynaming Programming implementation for sample input " + str(i))
-        Dynamic_Programming(N, stocks_and_values, Amount)
+        Dynamic_Programming(n, stocks, amount)
 
         print("=================")
 
-def Exhaustive_Search(N, stocks, Amount):
+def Exhaustive_Search(n, stocks, amount):
     pass
 
-def Dynamic_Programming(N, stocks, Amount):
+def Dynamic_Programming(n, stocks, amount):
     pass
 
 Parse_File()
