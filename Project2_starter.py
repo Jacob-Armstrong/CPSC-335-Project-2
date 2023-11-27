aa8a4cfc1c914444464dd3bbcf72a77b47bc60e7
Jacob <silverlock99@gmail.com>
2023-11-26 23:33:06 -0800

    Modifications for formatting

--- Project2_starter.py
+++ Project2_starter.py
@@ -1,25 +1,29 @@
 from itertools import combinations
 
 def Dynamic_Programming(n, stocks_and_values, amount):
-    dp = [0] * (amount + 1)
+    dp = [(0, []) for _ in range(amount + 1)]
 
     for stock in stocks_and_values:
         for j in range(amount, stock[1] - 1, -1):
-            dp[j] = max(dp[j], stock[0] + dp[j - stock[1]])
+            if stock[0] + dp[j - stock[1]][0] > dp[j][0]:
+                dp[j] = (stock[0] + dp[j - stock[1]][0], dp[j - stock[1]][1] + [stock])
 
     return dp[amount]
 
 def Exhaustive_Search(n, stocks_and_values, budget):
-    stock_count = 0
+    max_value = 0
+    selected_stocks = []
 
     for r in range(1, n + 1):
         for stocks_n_values in combinations(stocks_and_values, r):
             total = sum(stock[1] for stock in stocks_n_values)
             if total <= budget:
                 stocks = sum(stock[0] for stock in stocks_n_values)
-                stock_count = max(stock_count, stocks)
+                if stocks > max_value:
+                    max_value = stocks
+                    selected_stocks = list(stocks_n_values)
 
-    return stock_count
+    return max_value, selected_stocks
 
 def Parse_File():
     with open('input.txt', 'r') as file:
@@ -28,6 +32,7 @@ def Parse_File():
     sections = content.split('Sample input')[1:]
 
     for i, section in enumerate(sections, start=1):
+        # File Parsing
         n_index = section.find('N = ')
         n_end = section.find('\n', n_index)
         n = int(section[n_index + len('N = '):n_end].strip())
@@ -46,19 +51,31 @@ def Parse_File():
           amount_end = section.find('\n', amount_index)
         amount = int(section[amount_index + len('Amount = '):amount_end].strip())
 
-        print(f"Sample {i}:")
+        # File Output
+        if i > 9:
+          print("=============")
+          print(f"= Sample {i} =")
+          print("=============\n")
+        else:
+          print("============")
+          print(f"= Sample {i} =")
+          print("============\n")
+
         print("N:", n)
         print("Stocks and Values:", stocks)
-        print("Amount:", amount)
-        print("------")
+        print("Amount:", amount, "\n")
+        #print("--------------------")
 
-        print("Beginning Dynamic Programming implementation for sample input " + str(i))
-        result = Dynamic_Programming(n, stocks, amount)
-        print("Result:", result)
-
-        print("Beginning Exhaustive Search implementation for sample input " + str(i))
+        print("<< Part A: Exhaustive Search >>")
         es_result = Exhaustive_Search(n, stocks, amount)
-        print("Result:", es_result)
-        print("=================")
+        max_value, selected_stocks = es_result
+        print("Maximum value:", max_value)
+        print("Selected stocks:", selected_stocks, "\n")
+
+        print("<< Part B: Dynamic Programming >>")
+        result = Dynamic_Programming(n, stocks, amount)
+        max_value, selected_stocks = result
+        print("Maximum value:", max_value)
+        print("Selected stocks:", selected_stocks, "\n")
 
 Parse_File()
